dab304dd42163c75a893cb58651c398e1d66be81
Mayank Mehta <mayankmehta880@gmail.com>
2024-09-22 06:20:35 +0530

    complete UI of Main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./Main.css";
+import { assets } from '../../assets/assets';
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="" />
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Mayank</span></p>
+                    <p>How can i help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. ero quos doloribus!</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum, dolor sit amet consectetur doloribus!</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, accusamus nemo optio beatae non libero quos doloribus!</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.  doloribus!</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                    Gemini may display inaccurate info, including about people, so double-check its responses.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
