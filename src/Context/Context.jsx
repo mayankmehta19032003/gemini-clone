0675d2d1a66797f962c31e21be10fea8c03e09d2
Mayank Mehta <mayankmehta880@gmail.com>
2024-09-23 07:50:08 +0530

    setting all css for main for showing result

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -4,23 +4,40 @@ import run from "../config/gemini";
 export const context = createContext();
 
 
-const ContextProvider = (props) =>{
+const ContextProvider = (props) => {
 
-    const [input,setInput] = useState("");
-    const [recentPrompt,setRecentPrompt] = useState("");
-    const [prevPrompts,setPrevPrompts] = useState("");
-    const [showResult,setShowResult] = useState(false);
-    const [loading,setLoading] = useState(false);
-    const [resultData,setResultData] = useState("");
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState("");
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
 
 
 
-    const onSent = async (prompt)=>{
-        await run(prompt)
+    const onSent = async (prompt) => {
+
+        setResultData("")
+        setLoading(true)
+        setShowResult(true);
+        setRecentPrompt(input);
+        const respone = await run(input)
+        setResultData(respone)
+        setLoading(false)
+        setInput("")
     }
 
     const contextValue = {
-     
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        input,
+        setInput,
+        showResult,
+        loading,
+        resultData
     }
 
 
--- src/config/gemini.js
+++ src/config/gemini.js
@@ -39,6 +39,7 @@ import {
   
     const result = await chatSession.sendMessage(prompt);
     console.log(result.response.text());
+    return result.response.text();
   }
   
   export default run;
